1bd1ebf9b344bb1d0ef7eb68384d944792898c06
faridal12004 <faridagsuhendry@gmail.com>
2022-03-08 09:16:26 +0700

    pull

--- ruang-admin/edit-profil-dinaskab.php
+++ ruang-admin/edit-profil-dinaskab.php
@@ -62,7 +62,7 @@ html,body,h1,h2,h3,h4,h5 {}
       </a>
       <hr class="sidebar-divider my-0">
       <li class="nav-item active">
-        <a class="nav-link" href="dashboard-dinasprov.html">
+        <a class="nav-link" href="dashboard-dinaskabkota.html">
         <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
@@ -164,7 +164,7 @@ html,body,h1,h2,h3,h4,h5 {}
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 <img class="img-profile rounded-circle" src="img/boy.png" style="max-width: 60px">
-                <span class="ml-2 d-none d-lg-inline text-white small">admindinasprov</span>
+                <span class="ml-2 d-none d-lg-inline text-white small">admindinaskab</span>
               </a>
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="" href="">
